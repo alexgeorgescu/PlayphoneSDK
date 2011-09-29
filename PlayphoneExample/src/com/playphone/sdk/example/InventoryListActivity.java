97800bc502cd7707a7d90649ec656f4525065f70
alexgeorgescu <alex_georgescu_dba@yahoo.com>
2011-09-28 20:01:59 -0700

    Fixed inventory management

--- PlayphoneExample/src/com/playphone/sdk/example/InventoryListActivity.java
+++ PlayphoneExample/src/com/playphone/sdk/example/InventoryListActivity.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.playphone.multinet.MNDirect;
+import com.playphone.multinet.providers.MNVItemsProvider.GameVItemInfo;
 import com.playphone.multinet.providers.MNVItemsProvider.PlayerVItemInfo;
 
 import android.content.Intent;
@@ -24,12 +25,42 @@ public class InventoryListActivity extends CustomTitleActivity {
 		TextView txtBreadCrumbs = (TextView) findViewById(R.id.txtBreadCrumbs);
 		txtBreadCrumbs.setText("Home > Virtual Economy > Inventory > Inventory List");
 		
-		List<PlayerVItemInfo> playerItems = Arrays.asList(MNDirect.getVItemsProvider().getPlayerVItemList());
-		
-		
-		
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
 		LinearLayout layoutCurrencies = (LinearLayout) findViewById(R.id.layoutCurrencies);
 		LinearLayout layoutItems = (LinearLayout) findViewById(R.id.layoutItems);
+		layoutCurrencies.removeAllViews();
+		layoutItems.removeAllViews();
+		List<PlayerVItemInfo> playerItems = Arrays.asList(MNDirect.getVItemsProvider().getPlayerVItemList());
+		for(PlayerVItemInfo playerItem : playerItems)
+		{
+			int itemId = playerItem.id;
+			long amount = playerItem.count;
+			GameVItemInfo gameItem = MNDirect.getVItemsProvider().findGameVItemById(itemId);
+			String name = gameItem.name;
+			String txtToDisplay = "ID" + String.valueOf(itemId) + " " + name + "      " + String.valueOf(amount);
+			TextView txtItem = new TextView(this);
+			txtItem.setText(txtToDisplay);
+			
+			// the model of a virtual item is a flag, 
+			// so we can bitwise compare with 1 in this case,
+			// since flag 1 is currency
+			if((Integer.valueOf(gameItem.model) & 1) == 0)
+			{
+				//not currency, add to layoutItems
+				layoutItems.addView(txtItem);
+			}
+			else
+			{
+				//currency, add to layoutCurrencies
+				layoutCurrencies.addView(txtItem);
+			}
+		}
+		layoutCurrencies.invalidate();
+		layoutItems.invalidate();
 	}
 
 
--- PlayphoneExample/src/com/playphone/sdk/example/InventoryManageActivity.java
+++ PlayphoneExample/src/com/playphone/sdk/example/InventoryManageActivity.java
@@ -1,17 +1,38 @@
 package com.playphone.sdk.example;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.playphone.multinet.MNDirect;
+import com.playphone.multinet.providers.MNVItemsProvider;
+import com.playphone.multinet.providers.MNVItemsProvider.GameVItemInfo;
 import com.playphone.multinet.providers.MNVItemsProvider.PlayerVItemInfo;
+import com.playphone.multinet.providers.MNVItemsProvider.TransactionError;
+import com.playphone.multinet.providers.MNVItemsProvider.TransactionInfo;
+import com.playphone.multinet.providers.MNVShopProvider.VShopPackInfo;
 
-public class InventoryManageActivity extends CustomTitleActivity {
+public class InventoryManageActivity extends CustomTitleActivity implements OnClickListener, Callback {
 
+	Map<String,Integer> itemMap = new HashMap<String,Integer>();
+	Handler handler = new Handler(this);
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,12 +42,101 @@ public class InventoryManageActivity extends CustomTitleActivity {
 		TextView txtBreadCrumbs = (TextView) findViewById(R.id.txtBreadCrumbs);
 		txtBreadCrumbs.setText("Home > Virtual Economy > Inventory > Manage Inventory");
 		
-		List<PlayerVItemInfo> playerItems = Arrays.asList(MNDirect.getVItemsProvider().getPlayerVItemList());
+		List<GameVItemInfo> gameItems = Arrays.asList(MNDirect.getVItemsProvider().getGameVItemsList());
+		for (GameVItemInfo gameItem : gameItems)
+		{
+			itemMap.put(gameItem.name, gameItem.id);
+		}
 		
 		
+		Spinner spinner = (Spinner) findViewById(R.id.spinnerItem);
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.array_adapter_layout);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		for(String key : itemMap.keySet())
+			adapter.add(key);
+	    spinner.setAdapter(adapter);
 		
-		//LinearLayout layoutCurrencies = (LinearLayout) findViewById(R.id.layoutCurrencies);
-		//LinearLayout layoutItems = (LinearLayout) findViewById(R.id.layoutItems);
+		
+		Button btnAdd = (Button) findViewById(R.id.btnAdd);
+		btnAdd.setOnClickListener(this);
+		
+		Button btnSubstract = (Button) findViewById(R.id.btnSubstract);
+		btnSubstract.setOnClickListener(this);
+		
+		
+		MNDirect.getVItemsProvider().addEventHandler(new MNVItemsProvider.EventHandlerAbstract()
+		 {
+		  public void onVItemsTransactionCompleted (TransactionInfo  transaction)
+		   {
+			  //transaction succeeded, update the application
+			  Log.d("playphone","Transaction " + transaction.clientTransactionId + " succeeded.");
+			  Message msg = new Message();
+			  Bundle bundle = new Bundle();
+			  bundle.putString("message", "Transaction was successful");
+			  msg.setData(bundle);
+			  handler.sendMessage(msg);
+		   }
+		 
+		  public void onVItemsTransactionFailed (TransactionError error)
+		   {
+			  //transaction succeeded, update the application
+			  Log.d("playphone","Transaction " + error.clientTransactionId + " failed.");
+			  Message msg = new Message();
+			  Bundle bundle = new Bundle();
+			  bundle.putString("message", "Transaction failed with error" + error.errorMessage);
+			  msg.setData(bundle);
+			  handler.sendMessage(msg);
+		   }
+		 });
+		
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		switch( v.getId())
+		{
+		case R.id.btnAdd:
+			EditText editAdd = (EditText) findViewById(R.id.editAdd);
+			changeItem(Integer.valueOf(editAdd.getText().toString()));
+			break;
+		case R.id.btnSubstract:
+			EditText editSub = (EditText) findViewById(R.id.editSub);
+			changeItem(0 - Integer.valueOf(editSub.getText().toString()));
+			break;
+		default:
+		}
+	}
+	
+	private void changeItem(int count)
+	{
+		
+		if(MNDirect.isUserLoggedIn())
+		{
+			Spinner spinner = (Spinner) findViewById(R.id.spinnerItem);
+			int itemToBuy = itemMap.get(spinner.getSelectedItem().toString());
+			MNDirect.getVItemsProvider().reqAddPlayerVItem(itemToBuy,count,
+				MNDirect.getVItemsProvider().getNewClientTransactionId());
+			Toast.makeText(this, "Trying to change your item " +  spinner.getSelectedItem().toString() 
+					+ " by " + String.valueOf(count), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		Log.d("playphone","Received new message");
+		Bundle bundle = msg.getData();
+		if(bundle.containsKey("message"))
+		{
+			Toast.makeText(this, bundle.getString("message"), Toast.LENGTH_SHORT).show();
+			Spinner spinner = (Spinner) findViewById(R.id.spinnerItem);
+			int itemChanged = itemMap.get(spinner.getSelectedItem().toString());
+			TextView txtAmount = (TextView) findViewById(R.id.txtAmount);
+			txtAmount.setText(String.valueOf(MNDirect.getVItemsProvider().getPlayerVItemCountById(itemChanged)));
+			txtAmount.invalidate();
+		}
+		return false;
 	}
 
 }
